5b906b5bc38a6f812b98405b6198a8f623c79904
Codeitout8103468296 <siddhi.khare.cer22@itbhu.ac.in>
2024-03-08 17:49:21 +0530

    Major Changes

--- src/MainSection/Main.js
+++ src/MainSection/Main.js
@@ -3,37 +3,46 @@ import Box from './Box';
 import './main.css';
 import Loading from '../Loading/Loading';
 import InfiniteScroll from 'react-infinite-scroll-component';
-function Main(props) {
+function Main({category, country}) {
 
+  
   const [news, setNews] = useState({ "articles": [] });
   const [page, setpage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   useEffect(() => {
-    document.title = props.category.charAt(0).toUpperCase() + props.category.slice(1) + "-TimeTeller";
-    const fetchData = async () => {
-      try {
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${page}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        setNews(data);
-      } catch (error) {
-        console.log("Error fetching data", error);
-      }
-    };
-    fetchData();
-  }, [props]);
+    fetchMoreData(true);
+  }, [page]);
+  useEffect(()=>{
+    fetchMoreData(false);
+  },[category, country])
+  useEffect(()=>{
+    document.title = category.charAt(0).toUpperCase() + category.slice(1) + "-TimeTeller";
+    fetchMoreData(false);
+  },[])
 
-
-  const fetchMoreData = async () => {
+  const fetchMoreData = async (append) => {
     try {
-      setpage(page + 1);
-      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_API_KEY}&page=${page + 1}`;
-      const response = await fetch(url);
+      const scope={category,country, page};
+      const response = await fetch(process.env.REACT_APP_BACKEND_URL+"news",{
+        method:"POST",
+        body:JSON.stringify(scope),
+        headers:{
+          "Content-Type":"application/json"
+        }
+      });
       const data = await response.json();
+      if(data.articles.length === 0) {
+        setHasMore(false);
+      }
+      if(append)
       setNews((prevNews) => ({
         ...prevNews,
         articles: [...prevNews.articles, ...data.articles],
         totalResults: data.totalResults
       }));
+      else {
+        setNews(data)
+      }
     } catch (error) {
       console.log("Error fetching more data", error);
     }
@@ -47,12 +56,15 @@ function Main(props) {
   return (<div style={{marginTop:"4rem"}}>
     <div style={{ margin: "1rem" }}>
       <div style={{ fontSize: "3rem", fontWeight: "900", fontStyle: "italic", textDecoration: "underline" }}>-:TimeTeller:-</div>
-      <div style={{ fontSize: "1rem" }}>Timely Updates . Trusted News{news.totalResults}</div>
+      <div style={{ fontSize: "1rem" }}>Timely Updates . Trusted News</div>
     </div>
     <InfiniteScroll
       dataLength={news.articles.length}
-      next={fetchMoreData}
-      hasMore={news.articles.length !== news.totalResults}
+      next={()=>{
+        setpage(p=>p+1)
+        console.log("newpage!!!")
+      }}
+      hasMore={hasMore}
       loader={<Loading />}
     >
       <div className='main'>
@@ -60,6 +72,7 @@ function Main(props) {
         {news.articles && news.articles.map((newsItem) => (
           <Box key={newsItem.url} {...newsItem} />
         ))}
+       
       </div>
     </InfiniteScroll>
 
